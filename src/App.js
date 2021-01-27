66440d0c5de9f345326a0293835e92896debd678
Mina Ghobrial <minamaged113@gmail.com>
2021-01-28 00:27:15 +0200

    import TodoList component into the main app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 // importing components
 import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
 
 function App() {
   return (
     <div className="App">
       <header>Mina's ToDo List</header>
       <Form />
+      <TodoList />
     </div>
   );
 }
