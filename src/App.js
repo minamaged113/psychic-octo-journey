0888c20e9d4cea954a09997b8832e316f53611eb
Mina Ghobrial <minamaged113@gmail.com>
2021-01-28 00:21:04 +0200

    use the designed form into the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+// importing components
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
       <header>Mina's ToDo List</header>
+      <Form />
     </div>
   );
 }
