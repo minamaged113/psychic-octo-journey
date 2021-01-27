78488d8750ce792b2ab6be51386f619f5f36a0d9
Mina Ghobrial <minamaged113@gmail.com>
2021-01-28 00:45:28 +0200

    import useState and create two states

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import './App.css';
 // importing components
 import Form from "./components/Form";
@@ -5,6 +6,7 @@ import TodoList from "./components/TodoList";
 
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <header>Mina's ToDo List</header>
