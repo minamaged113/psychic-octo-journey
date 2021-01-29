0e4abf62ffec6b98140115d39114fa10a7d2d1fe
Mina Ghobrial <minamaged113@gmail.com>
2021-01-29 19:05:32 +0200

    change todo list based on drop-down choice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 // importing components
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  // states
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  // use effect
+  useEffect(() => {
+    filterHandler();
+  },
+  [todos, status]); // every time any of those is changed...
+  // this function is run
+  // functions and events
+
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+
   return (
     <div className="App">
       <header>Mina's ToDo List</header>
@@ -20,7 +45,7 @@ function App() {
         setInputText={setInputText}
         setStatus={setStatus}
       />
-      <TodoList setTodos={setTodos} todos={todos} />
+      <TodoList setTodos={setTodos} todos={todos} filteredTodos={filteredTodos} />
     </div>
   );
 }
