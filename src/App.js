c94fe0110392b0461d96f7d200b3b8f73b4229b4
Mina Ghobrial <minamaged113@gmail.com>
2021-01-29 11:57:25 +0200

    update status with value of dropdown list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 // importing components
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
-
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
   return (
     <div className="App">
       <header>Mina's ToDo List</header>
       {/* pass the function to the Form component 
       This will be accessed from Form component*/}
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
-      <TodoList setTodos={setTodos} todos={todos}/>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
