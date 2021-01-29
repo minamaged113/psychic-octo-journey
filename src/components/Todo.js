971ac73b3c58961c6f4925c7532e0a64426dbc4e
Mina Ghobrial <minamaged113@gmail.com>
2021-01-29 11:49:23 +0200

    set tasks as completed by marking done sign

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,10 +7,20 @@ const Todo = ({text, todo, todos, setTodos}) => {
         setTodos(todos.filter((el) => el.id !== todo.id));
         // console.log(todo);
     };
+    const completeHandler = () =>{
+        setTodos(todos.map((item) => {
+            if (item.id === todo.id) {
+                return {
+                    ...item, completed: !item.completed
+                }
+            }
+            return item;
+        }));
+    };
     return (
         <div className="todo">
-            <li className="todo-item">{text}</li>
-            <button className="complete-btn"><i className="fas fa-check"></i></button>
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+            <button onClick={completeHandler} className="complete-btn"><i className="fas fa-check"></i></button>
             <button onClick={deleteHandler} className="trash-btn"><i className="fas fa-trash"></i></button>
         </div>
     );
